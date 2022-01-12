bab7349f2aff03fd9f372d1c9a2ad3d267bdd415
mrjordanov <marin.jordanov@gmail.com>
2022-01-12 16:02:25 +0200

    New Tasks
    add new solutions

--- 3.SetsAndMapsAdvanced/src/AvvStudentsGrades.java
+++ 3.SetsAndMapsAdvanced/src/AvvStudentsGrades.java
@@ -1,11 +1,40 @@
+
+
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+
 
 public class AvvStudentsGrades {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
 
+        TreeMap<String, LinkedList<Double>> students = new TreeMap<>();
 
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] arg = line.split(" ");
+            String name = arg[0];
+            double grade = Double.parseDouble(arg[1]);
 
+            students.putIfAbsent(name, new LinkedList<>());
+            LinkedList<Double> grades = students.get(name);
+            grades.add(grade);
+
+        }
+
+        for (var entry : students.entrySet()) {
+            LinkedList<Double> grades = entry.getValue();
+            double avv = (grades.stream().mapToDouble(Double::doubleValue).average().getAsDouble());
+            System.out.print(entry.getKey() + " -> ");
+            grades.forEach(e -> {
+                System.out.printf("%.2f ", e);
+            });
+            System.out.printf("(avg: %.2f)", avv).println();
+        }
 
     }
 }
--- 3.SetsAndMapsAdvanced/src/CitiesByContinentAndCountry.java
+++ 3.SetsAndMapsAdvanced/src/CitiesByContinentAndCountry.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 public class CitiesByContinentAndCountry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Map<String, LinkedHashMap<String, LinkedList<String>>> information = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] arg = line.split(" ");
+            String continent = arg[0];
+            String land = arg[1];
+            String city = arg[2];
+
+            information.putIfAbsent(continent, new LinkedHashMap<>());
+            LinkedHashMap<String, LinkedList<String>> country = information.get(continent);
+            country.putIfAbsent(land, new LinkedList<>());
+            LinkedList<String> cities = country.get(land);
+            cities.add(city);
+        }
+
+        information.entrySet().stream()
+                .forEach(e -> {
+                    System.out.println(e.getKey() + ":");
+                    e.getValue().entrySet().stream()
+                            .forEach(ie -> {
+                                System.out.println("  " + ie.getKey() + " -> " + String.join(", ", ie.getValue()));
+                            });
+                });
     }
 }
