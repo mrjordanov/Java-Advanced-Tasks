17a78c2e06b19ebfda2238ac741f586bcdc99f9e
mrjordanov <marin.jordanov@gmail.com>
2022-01-14 16:35:32 +0200

    Last task
    add last 8th task solution

---
+++ 4.Files and Streams/src/ListFiles.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class ListFiles {
+    public static void main(String[] args) throws IOException {
+
+        File root= new File("C:\\Users\\1Marin1\\Downloads\\SoftUni\\Advanced\\" +
+                "4.Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        Path outPath= Paths.get("C:\\Users\\1Marin1\\Desktop\\Java-Advanced-Tasks\\4.Files and Streams\\Lits-Files.txt");
+
+        File[] files = root.listFiles();
+        List<String>info= new LinkedList<>();
+        for (File f:files) {
+            if(f.isFile()){
+                String name=f.getName();
+                long size=f.length();
+                String sizeOfFile=""+size+"";
+                String format= name+": ["+sizeOfFile+"]";
+                info.add(format);
+            }
+        }
+
+        Files.write(outPath,info);
+
+    }
+}
---
+++ 4.Files and Streams/src/NestedFolders.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class NestedFolders {
+    public static void main(String[] args) {
+
+        File root= new File ("C:\\Users\\1Marin1\\Downloads\\SoftUni\\Advanced\\4.Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        Deque<File> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        int count=1;
+        while (!queue.isEmpty()) {
+            File file = queue.poll();
+            File[] files = file.listFiles();
+                for (File f : files) {
+                    if (f.isDirectory()) {
+                        queue.offer(f);
+                        count++;
+                        System.out.println(f.getName());
+                    }
+                }
+        }
+
+        System.out.println(count+" folders");
+    }
+}
