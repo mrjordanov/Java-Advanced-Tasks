86549c8de855375fc41e9dd05b2770ae1773dcea
mrjordanov <marin.jordanov@gmail.com>
2022-01-14 13:57:49 +0200

    Add 5th taks

---
+++ 4.Files and Streams/src/WriteEvery3thLine.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class WriteEvery3thLine {
+    public static void main(String[] args) throws IOException {
+
+        String path="input.txt";
+        FileInputStream inputStream = new FileInputStream(path);
+        InputStreamReader inputStreamReader= new InputStreamReader(inputStream);
+        BufferedReader reader= new BufferedReader(inputStreamReader);
+
+        BufferedWriter writer= new BufferedWriter(new PrintWriter("Print #th Line.txt"));
+
+        String line=reader.readLine();
+        int count=1;
+
+        while (line!=null){
+            if (count%3==0){
+                writer.write(line+System.lineSeparator());
+                System.out.println(line);
+            }
+            line= reader.readLine();
+            count++;
+        }
+
+        writer.close();
+    }
+}
