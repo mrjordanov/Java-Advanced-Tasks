14093a3e0b1eaa12fc536e947f2d1201328d6299
mrjordanov <marin.jordanov@gmail.com>
2022-01-17 17:05:00 +0200

    Add 6th task
    Add 6th task solution

--- 4.Files and Streams/src/WordCount.java
+++ 4.Files and Streams/src/WordCount.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,15 +11,15 @@ public class WordCount {
         "4.Streams, Files and Directories\\LAB\\" +
                 "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\words.txt";
 
-        List<String> words= new ArrayList<>();
         BufferedReader reader= new BufferedReader(new FileReader(path));
-
+        BufferedWriter writer= new BufferedWriter(new PrintWriter("Result.txt"));
         Path inPath= Paths.get(path);
         List<String > word=Files.readAllLines(inPath);
 
+        List<String> wordsToFind= new ArrayList<>();
         for (String wor:word) {
-            String[] arr=wor.toLowerCase(Locale.ROOT).split(" ");
-            words.addAll(Arrays.asList(arr));
+            String[] arr=wor.split(" ");
+            wordsToFind.addAll(Arrays.asList(arr));
         }
 
         String sec="C:\\Users\\1Marin1\\Downloads\\SoftUni\\Advanced\\" +
@@ -29,23 +27,41 @@ public class WordCount {
                 "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\text.txt";
 
         Path second=Paths.get(sec);
-        List<String> lines=Files.readAllLines(second);
+        List<String> linesOfText=Files.readAllLines(second);
+        List<String > wordsOfText=new ArrayList<>();
+        for (String oneWord:linesOfText) {
+            String[] arrWithWords=oneWord.split(" ");
+                    wordsOfText.addAll(Arrays.asList(arrWithWords));
+        }
+
 
         Map<String,Integer> occ= new LinkedHashMap<>();
 
-        for (int i = 0; i < words.size() ; i++) {
-            if (lines.toString().toLowerCase(Locale.ROOT).contains(words.get(i))){
-                while (lines.toString().toLowerCase(Locale.ROOT).contains(words.get(i))){
-                    occ.putIfAbsent(words.get(i),0);
-                    int count= occ.get(words.get(i))+1;
-                    occ.put(words.get(i),count);
-                   String put= lines.toString().replace(words.get(i),"");
+        for (int i = 0; i < wordsToFind.size() ; i++) {
+            String putInMap=wordsToFind.get(i);
 
+            for (String s : wordsOfText) {
+                String current = s;
+                if (current.equals(putInMap)){
+                    occ.putIfAbsent(putInMap, 0);
+                    int totalCount = occ.get(putInMap) + 1;
+                    occ.put(putInMap, totalCount);
                 }
             }
         }
 
 
 
+       occ.entrySet().stream().sorted((s1,s2)-> s2.getValue().compareTo(s1.getValue()))
+               .forEach(e-> {
+                   try {
+                       writer.write(e.getKey()+" - "+e.getValue());
+                       writer.newLine();
+                   } catch (IOException ex) {
+                       ex.printStackTrace();
+                   }
+               });
+
+        writer.close();
     }
 }
