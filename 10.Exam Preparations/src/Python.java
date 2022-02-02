d190b85155ee5a715e9b7f03d656b165135e794a
mrjordanov <marin.jordanov@gmail.com>
2022-02-02 17:08:05 +0200

    Python task
    2 solutions

--- 10.Exam Preparations/src/Python.java
+++ 10.Exam Preparations/src/Python.java
@@ -17,87 +17,95 @@ public class Python {
         List<int[]> enemiesIndexes = getEnemiesIndexes(matrix);
 
 
-        int curRowIndex = startIndex[0];
-        int curColIndex = startIndex[1];
-        int eatenFood=0;
+        int eatenFood = 0;
         for (int i = 0; i < commands.length; i++) {
             String command = commands[i];
-            int startRowIndex=startIndex[0];
-            int startColIndex=startIndex[1];
+            int curRowIndex = startIndex[0];
+            int curColIndex = startIndex[1];
             if (command.equals("up")) {
-                if (curRowIndex - 1 < 0) {
+                curRowIndex = startIndex[0] - 1;
+                if (curRowIndex < 0) {
                     curRowIndex = cols - 1;
                 }
                 if (isEnemiesIndexHere(curRowIndex, curColIndex, enemiesIndexes)) {
                     System.out.println("You lose! Killed by an enemy!");
                     return;
                 }
-                if (isFoodIndexHere(curRowIndex,curColIndex,foodIndexes)){
+                if (isFoodIndexHere(curRowIndex, curColIndex, foodIndexes)) {
                     eatenFood++;
-                    if(eatenFood==foodIndexes.size()){
-                        System.out.println("You win! Final python length is "+eatenFood+1);
-                       return;
+                    if (eatenFood == foodIndexes.size()) {
+                        System.out.println("You win! Final python length is " + (eatenFood + 1));
+                        return;
                     }
                 }
             }
             if (command.equals("down")) {
-                if (curRowIndex + 1 >= cols) {
+                curRowIndex = startIndex[0] + 1;
+                if (curRowIndex >= cols) {
                     curRowIndex = 0;
+
                 }
                 if (isEnemiesIndexHere(curRowIndex, curColIndex, enemiesIndexes)) {
                     System.out.println("You lose! Killed by an enemy!");
                     return;
                 }
-                if (isFoodIndexHere(curRowIndex,curColIndex,foodIndexes)){
+                if (isFoodIndexHere(curRowIndex, curColIndex, foodIndexes)) {
                     eatenFood++;
-                    if(eatenFood==foodIndexes.size()){
-                        System.out.println("You win! Final python length is "+eatenFood+1);
+                    if (eatenFood == foodIndexes.size()) {
+                        System.out.println("You win! Final python length is " + (eatenFood + 1));
                         return;
                     }
                 }
             }
             if (command.equals("left")) {
-                if (curColIndex - 1 < 0) {
+                curColIndex = startIndex[1] - 1;
+                if (curColIndex < 0) {
                     curColIndex = rows - 1;
+
                 }
                 if (isEnemiesIndexHere(curRowIndex, curColIndex, enemiesIndexes)) {
                     System.out.println("You lose! Killed by an enemy!");
                     return;
                 }
-                if (isFoodIndexHere(curRowIndex,curColIndex,foodIndexes)){
+                if (isFoodIndexHere(curRowIndex, curColIndex, foodIndexes)) {
                     eatenFood++;
-                    if(eatenFood==foodIndexes.size()){
-                        System.out.println("You win! Final python length is "+eatenFood+1);
+                    if (eatenFood == foodIndexes.size()) {
+                        System.out.println("You win! Final python length is " + (eatenFood + 1));
                         return;
                     }
                 }
             }
             if (command.equals("right")) {
-                if (curColIndex + 1 >= rows) {
+                curColIndex = startIndex[1] + 1;
+                if (curColIndex >= rows) {
                     curColIndex = 0;
+
                 }
                 if (isEnemiesIndexHere(curRowIndex, curColIndex, enemiesIndexes)) {
                     System.out.println("You lose! Killed by an enemy!");
                     return;
                 }
-                if (isFoodIndexHere(curRowIndex,curColIndex,foodIndexes)){
+                if (isFoodIndexHere(curRowIndex, curColIndex, foodIndexes)) {
                     eatenFood++;
-                    if(eatenFood==foodIndexes.size()){
-                        System.out.println("You win! Final python length is "+eatenFood+1);
+                    if (eatenFood == foodIndexes.size()) {
+                        System.out.println("You win! Final python length is " + (eatenFood + 1));
                         return;
                     }
                 }
             }
+
+            startIndex[0] = curRowIndex;
+            startIndex[1] = curColIndex;
         }
 
-        int leftFood=foodIndexes.size()-eatenFood;
-        System.out.printf("You lose! There is still %d food to be eaten.",leftFood);
-        // printMatrix(matrix);
+        int leftFood = foodIndexes.size() - eatenFood;
+        System.out.printf("You lose! There is still %d food to be eaten.", leftFood);
+
     }
 
     private static boolean isFoodIndexHere(int curRowIndex, int curColIndex, List<int[]> foodIndexes) {
-        for (int[] elements:foodIndexes) {
-            if (elements[0]==curRowIndex && elements[1]==curColIndex){
+        for (int[] elements : foodIndexes) {
+            if (elements[0] == curRowIndex && elements[1] == curColIndex) {
                 return true;
             }
         }
